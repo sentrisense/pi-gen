d3b09cbafa26f639ae7a5bcede49c36c992146e7
Nacho Carballeda <icarballeda@sentrisense.com>
2024-08-01 16:50:34 -0300

    add esp idf export to bashrc

--- stage3/03-install-idf/00-run-chroot.sh
+++ stage3/03-install-idf/00-run-chroot.sh
@@ -14,3 +14,7 @@ cd esp-idf
 
 ./install.sh esp32 esp32s2 --enable-pytest
 
+# Add source line to bashrc
+on_chroot <<- EOF
+    echo '. /home/$FIRST_USER_NAME/esp-idf/export.sh' >> /home/${FIRST_USER_NAME}/.bashrc
+EOF

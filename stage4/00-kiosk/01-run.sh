52dc871012c9fdd5a4f922c44b1c65390a95f780
Nacho Carballeda <icarballeda@sentrisense.com>
2024-08-09 15:42:21 -0300

    add video output stage 4

---
+++ stage4/00-kiosk/01-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -xe
+
+for file in files/*; do
+    if [ -f "$file" ]; then
+        sed -i "s|example_kiosk_url|$KIOSK_URL|g" "$file"
+        sed -i "s|example_user|$FIRST_USER_NAME|g" "$file"
+    fi
+done
+
+install -v -m 777 files/xsession   "${ROOTFS_DIR}/home/$FIRST_USER_NAME/.xsession"
+install -v -m 600 files/autologin.conf   "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/"
+install -v -m 600 files/lightdm.conf   "${ROOTFS_DIR}/etc/lightdm/"
+
+install -v -m 600 files/ttyd.service            	"${ROOTFS_DIR}/etc/systemd/system/"
+
+on_chroot << EOF
+    systemctl enable ttyd.service
+EOF
+
